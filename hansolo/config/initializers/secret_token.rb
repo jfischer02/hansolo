8bae34e56d5aa312716bd6ef99fd82c2c8b1f5ab
Jeremy Fischer <jeremyfischer@new-host-2.home>
2013-02-21 22:17:28 -0500

    first commit to github baby!

---
+++ hansolo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hansolo::Application.config.secret_token = 'd72f61e7a1c9400d0f79e0d8cc935f13816c519557a8705de71f69a72a1639bb0567899d50680cd8317e67deccdf592e105251b261eb3f9f2a24535d12a1ce8e'
